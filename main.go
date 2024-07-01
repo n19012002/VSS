047ef2b665ce5d963f035ae7d59f9d15586795d8
Trương Xuân Nguyên <215748020110162@vinhuni.edu.vn>
2024-07-02 03:04:47 +0700

    haha

--- main.go
+++ main.go
@@ -8,15 +8,12 @@ import (
 	"strings"
 	"time"
 
+	"github.com/n19012002/brainfuck-psiphon-pro-go/src/libpsiphon"
 	"github.com/aztecrabbit/libinject"
 	"github.com/aztecrabbit/liblog"
 	"github.com/aztecrabbit/libproxyrotator"
 	"github.com/aztecrabbit/libredsocks"
 	"github.com/aztecrabbit/libutils"
-	"github.com/miekg/dns"
-	"github.com/miekg/v2ray-core/proxy/vmess"
-	"github.com/miekg/v2ray-core/proxy/vmess/outbound"
-	"github.com/miekg/v2ray-core/proxy/vmess/protocol"
 )
 
 const (
@@ -31,25 +28,18 @@ const (
 var (
 	InterruptHandler = new(libutils.InterruptHandler)
 	Redsocks         = new(libredsocks.Redsocks)
-	VmessClient      *outbound.Client
 )
 
 type Config struct {
 	ProxyRotator *libproxyrotator.Config
 	Inject       *libinject.Config
-	Vmess        []*VmessConfig // Sử dụng mảng để lưu trữ nhiều Vmess server
-}
-
-type VmessConfig struct {
-	Address string
-	Port    string
-	Id      string
-	AlterId int
-	Security string
+	PsiphonCore  int
+	Psiphon      *libpsiphon.Config
 }
 
 func init() {
 	InterruptHandler.Handle = func() {
+		libpsiphon.Stop()
 		libredsocks.Stop(Redsocks)
 		liblog.LogKeyboardInterrupt()
 	}
@@ -88,40 +78,40 @@ func main() {
 	}
 	defaultConfig.Inject.Payload = ""
 	defaultConfig.Inject.Timeout = 5
-	defaultConfig.Vmess = []*VmessConfig{
-		{
-			Address: "157.10.52.130",
-			Port:    "80",
-			Id:      "f8c182c3-b3ea-586c-9c29-8e7d664e4f16",
-			AlterId: 2,
-			Security: "auto",
-		},
-		// Thêm server mới vào đây
-		{
-			Address: "1.2.3.4",
-			Port:    "443",
-			Id:      "your_id_here",
-			AlterId: 8,
-			Security: "aes-128-gcm",
-		},
+	defaultConfig.PsiphonCore = 4
+	defaultConfig.Psiphon = libpsiphon.DefaultConfig
+
+	if runtime.GOOS == "windows" {
+		defaultConfig.Psiphon.CoreName += ".exe"
 	}
 
 	libutils.JsonReadWrite(GetConfigPath("config.json"), config, defaultConfig)
 
+	var flagPro = true
 	var flagRefresh = false
 	var flagVerbose = false
 	var flagFrontend string
 	var flagWhitelist string
 
+	flag.BoolVar(&flagPro, "pro", flagPro, "Pro Version?")
 	flag.BoolVar(&flagRefresh, "refresh", flagRefresh, "Refresh Data")
 	flag.BoolVar(&flagVerbose, "verbose", flagVerbose, "Verbose Log?")
 	flag.StringVar(&flagFrontend, "f", flagFrontend, "-f frontend-domains (e.g. -f cdn.com,cdn.com:443)")
 	flag.StringVar(&flagWhitelist, "w", flagWhitelist, "-w whitelist-request (e.g. -w akamai.net:80)")
 	flag.IntVar(&config.Inject.MeekType, "mt", config.Inject.MeekType, "-mt meek type (0 and 1 for fastly)")
+	flag.IntVar(&config.PsiphonCore, "c", config.PsiphonCore, "-c core (e.g. -c 4) (1 for Pro Version)")
+	flag.StringVar(&config.Psiphon.Region, "r", config.Psiphon.Region, "-r region (e.g. -r sg)")
+	flag.IntVar(&config.Psiphon.Tunnel, "t", config.Psiphon.Tunnel, "-t tunnel (e.g. -t 4) (1 for Reconnect Version)")
+	flag.IntVar(&config.Psiphon.TunnelWorkers, "tw", config.Psiphon.TunnelWorkers, "-tw tunnel-workers (e.g. -tw 6) (8 for Pro Version)")
+	flag.IntVar(&config.Psiphon.KuotaDataLimit, "l", config.Psiphon.KuotaDataLimit, "-l limit (in MB) (e.g. -l 4) (0 for Pro Version (unlimited))")
 	flag.Parse()
 
+	if !flagPro {
+		config.Psiphon.Authorizations = make([]string, 0)
+	}
+
 	if flagRefresh {
-		//libpsiphon.RemoveData()
+		libpsiphon.RemoveData()
 	}
 
 	if flagFrontend != "" || flagWhitelist != "" {
@@ -152,42 +142,35 @@ func main() {
 	liblog.LogInfo("Domain Fronting running on port "+Inject.Config.Port, "INFO", liblog.Colors["G1"])
 	liblog.LogInfo("Proxy Rotator running on port "+ProxyRotator.Config.Port, "INFO", liblog.Colors["G1"])
 
+	if _, err := os.Stat(libutils.RealPath(config.Psiphon.CoreName)); os.IsNotExist(err) {
+		liblog.LogInfo(
+			fmt.Sprintf(
+				"Exception:\n\n"+
+					"|	 File '%s' not exist!\n"+
+					"|	 Exiting...\n"+
+					"|\n",
+				config.Psiphon.CoreName,
+			),
+			"INFO", liblog.Colors["R1"],
+		)
+		return
+	}
+
 	Redsocks.Config = libredsocks.DefaultConfig
 	Redsocks.Config.LogOutput = GetConfigPath("redsocks.log")
 	Redsocks.Config.ConfigOutput = GetConfigPath("redsocks.conf")
 	Redsocks.Start()
 
-	// Tích hợp Vmess
-	for _, vmessConfig := range config.Vmess {
-		vmessConfig := &vmess.Config{
-			Addrs: []string{
-				fmt.Sprintf("%s:%s", vmessConfig.Address, vmessConfig.Port),
-			},
-		}
+	for i := 1; i <= config.PsiphonCore; i++ {
+		Psiphon := new(libpsiphon.Psiphon)
+		Psiphon.ProxyRotator = ProxyRotator
+		Psiphon.Config = config.Psiphon
+		Psiphon.ProxyPort = Inject.Config.Port
+		Psiphon.KuotaData = libpsiphon.DefaultKuotaData
+		Psiphon.ListenPort = libutils.Atoi(ProxyRotator.Config.Port) + i
+		Psiphon.Verbose = flagVerbose
 
-		if vmessConfig.AlterId <= 0 {
-			vmessConfig.AlterId = 1
-		}
-		v, err := protocol.NewVmessOutbound(vmessConfig.Id, vmessConfig.AlterId,
-			vmessConfig.Security, vmessConfig)
-		if err != nil {
-			liblog.LogInfo(fmt.Sprintf("Error creating Vmess Outbound: %s", err), "INFO", liblog.Colors["R1"])
-			return
-		}
-		VmessClient = outbound.NewClient(v, nil)
-
-		go func() {
-			dns.HandleFunc(":53", func(w dns.ResponseWriter, req *dns.Msg) {
-				liblog.LogVerbose(fmt.Sprintf("dns req: %+v", req), "DEBUG", liblog.Colors["CC"])
-				res := new(dns.Msg)
-				res.SetReply(req)
-				res.RecursionAvailable = false
-				VmessClient.Transport(res, req)
-				liblog.LogVerbose(fmt.Sprintf("dns res: %+v", res), "DEBUG", liblog.Colors["CC"])
-				w.WriteMsg(res)
-			})
-			dns.ListenAndServe(":53", "udp", nil)
-		}()
+		go Psiphon.Start()
 	}
 
 	InterruptHandler.Wait()
